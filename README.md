85bcd382fb1acbf7a4ad5d3ee7b6879c05b96b2e
derrick875 <tanweiming911@gmail.com>
2022-07-23 22:28:06 +0800

    trinity-landing-page
    trinity-landing-page

---
+++ README.md
@@ -0,0 +1,3 @@
+## Landing Page 
+
+## Trinity Protocol
---
+++ src/views/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import sections
+import Hero from '../components/sections/Hero';
+
+import Cta from '../components/sections/Cta';
+
+const Home = () => {
+
+  return (
+    <>
+      <Hero className="illustration-section-01" />
+      <Cta split />
+    </>
+  );
+}
+
+export default Home;
